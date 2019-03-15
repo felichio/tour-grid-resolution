34586cfd0a9d895b8e7b6e72b560b23c0d76b4f1
Felix <fsafaridis@hotmail.com>
2019-03-15 17:11:08 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/random/precache-manifest.f91293512af14283542b1221d179d606.js"
+  "/TourBTsimple/precache-manifest.32a3bbf959f3d17f5ae697b5ffeb097b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/random/index.html", {
+workbox.routing.registerNavigationRoute("/TourBTsimple/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
